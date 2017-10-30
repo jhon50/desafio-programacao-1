96502420e38861415cc2fa57d6170d4df0e802d5
Jhonatan Teixeira <jhonatan@fulllab.com.br>
2017-10-30 21:15:32 -0200

    upload action method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,16 @@ class HomeController < ActionController::Base
   before_action :authenticate_user!
 
   def index
+  end
 
+  def upload
+    begin
+      @file = params[:order_file]
+      order = OrderParser.parse_tab_separated_file @file
+      OrderService.persist(order)
+    rescue Exception
+      flash[:message] = 'Houve um erro ao importar o arquivo'
+      redirect_to home_index_path
+    end
   end
 end
